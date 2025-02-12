08742ed318457adec4c8cfb90b406e749c990dfc
cesarManjaro <cesarwamartinez@gmail.com>
2025-02-12 06:50:36 -0500

    Delete componente image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -76,14 +76,11 @@ export default function Home() {
                 key={item.id}
                 className="grid-item mx-4 duration-300 relative overflow-hidden shadow-lg group rounded-2xl z-10"
               >
-                <Image
-                  priority
+                <img
                   src={item.src.original}
                   alt={item.alt}
                   title={item.alt}
                   className="w-full h-full transition-transform duration-300 ease-in-out group-hover:scale-105 object-cover max-w-full max-h-full rounded-2xl"
-                  width={item.width}
-                  height={item.height}
                 />
                 <div className="absolute inset-0 flex flex-col justify-end bg-gradient-to-t from-black/100 to-transparent opacity-0 group-hover:opacity-100 z-40 transition-opacity duration-300 text-white p-4">
                   <h1>{item.photographer}</h1>
